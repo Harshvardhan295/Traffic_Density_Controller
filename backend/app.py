a73f6c8152025cb34fc5f1e361d35f110c2d4801
Harshvardhan295 <harshvardhankhajuria14@gmail.com>
2025-04-22 11:18:03 +0530

    updated CORS

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 # Configure CORS to allow requests from the frontend
 CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type", "Accept"]}})
 # Allow only your Netlify domain
-CORS(app, origins=["https://traffic-density-controller.netlify.app/"])
+CORS(app, resources={r"/*": {"origins": "https://traffic-density-controller.netlify.app"}})
 
 # Configure upload folder
 base_dir = os.path.dirname(os.path.abspath(__file__))
